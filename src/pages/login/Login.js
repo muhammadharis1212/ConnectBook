1b48c9fcf423ff28d2e51a692a2ce1873c914799
muhammadharis1212 <muhammadharis1212@gmail.com>
2022-10-29 06:58:17 -0700

    login page!

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Connect Book</h1>
+          <p>lorem ipsum </p>
+          <span>Don't Have an Account ?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
