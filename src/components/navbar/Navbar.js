390bb938da19543ddadeda24faad9f23b7f310ee
muhammadharis1212 <muhammadharis1212@gmail.com>
2022-10-31 07:52:18 -0700

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,20 +5,39 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import Brightness5OutlinedIcon from "@mui/icons-material/Brightness5Outlined";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="nav">
       <div className="left">
-        <Link to="/home" style={{ textDecoration: "none" }}>
+        <Link to="/" style={{ textDecoration: "none" }}>
           <span>Connect Book</span>
         </Link>
         <HomeOutlinedIcon />
         <DarkModeOutlinedIcon />
         <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsNoneOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/708440/pexels-photo-708440.jpeg"
+            alt=""
+          />
+          <span>Haris</span>
+        </div>
+      </div>
     </nav>
   );
 };
