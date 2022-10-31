847d5323878703c9200a631331957d369c1ccaec
muhammadharis1212 <muhammadharis1212@gmail.com>
2022-10-31 06:39:16 -0700

    created routes for the app

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Connect Book</h1>
+          <p>lorem ipsum </p>
+          <span>Don't Have an Account ?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
