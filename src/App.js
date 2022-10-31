c4aa7a796b4e54c52adbd1a07a4c2bf4bd0bfab3
muhammadharis1212 <muhammadharis1212@gmail.com>
2022-10-31 07:21:01 -0700

    Protected Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import LeftBar from "./components/leftbar/LeftBar";
 import RightBar from "./components/rightbar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = false;
   const Layout = () => {
     return (
       <div>
@@ -20,10 +28,28 @@ function App() {
     );
   };
 
+  const ProtectedRoute = (props) => {
+    if (!currentUser) return <Navigate to="/login" />;
+    return props.children;
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Login />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
     {
       path: "/login",
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./navBar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import Brightness5OutlinedIcon from "@mui/icons-material/Brightness5Outlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav">
+      <div className="left">
+        <Link to="/home" style={{ textDecoration: "none" }}>
+          <span>Connect Book</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+      </div>
+      <div className="right"></div>
+    </nav>
+  );
 };
 
 export default Navbar;
